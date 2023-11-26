a2e1f3c83ee4dd985d44b5e460a5d2f06470b42c
Kat W-E <kat@fonix.com>
2023-11-26 09:34:22 +0000

    adds popper and importmap to rails so javascript works with bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,2 @@
-import "@popperjs/core"
-import "bootstrap"// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require popper
+//= require bootstrap
