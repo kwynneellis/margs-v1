bcca80269f7811641d98eaae41cb69fefc4f8e61
Kat W-E <kat@fonix.com>
2023-12-05 22:02:37 +0000

    adds admin to users table. Adds name fields to sign up forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_05_215026) do
+ActiveRecord::Schema.define(version: 2023_12_05_215954) do
 
   create_table "enquiries", force: :cascade do |t|
     t.date "check_in"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2023_12_05_215026) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
