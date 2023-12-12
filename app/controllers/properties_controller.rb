36fb89d7807d332c984f1b78317126a75a7d618a
Kat W-E <kat@fonix.com>
2023-12-12 16:27:35 +0000

    creates edit properyt functionality. makes show page the root_path. moves things_to_know to property views

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,6 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: [:show]
+  before_action :set_user, only: %i[show edit update]
+  before_action :set_property, only: %i[show edit update]
 
   def index
     @properties = Property.all
@@ -8,9 +9,26 @@ class PropertiesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @property.update(property_params)
+
+    redirect_to root_path
+  end
+
   private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
 
   def set_property
     @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:address, :title, :description, :description2, :price_per_night, :minimum_nights, :cleaning_fee, :max_guests, :bedrooms, :bathrooms, :pet_friendly)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root to: "properties#show", id: 1
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
   get "recommendations", to: "pages#recommendations"
 
-  resources :properties, only: [:index, :show] do
+  resources :properties, only: [:index, :show, :edit, :update] do
     resources :enquiries, only: [:new, :create, :index]
   end
 end
