286ccae5370351600703b9e3eb688c439a874bae
Kat W-E <kat@fonix.com>
2023-12-12 16:28:25 +0000

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_12_12_161831) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "enquiries", force: :cascade do |t|
     t.date "check_in"
     t.date "check_out"
@@ -23,7 +20,7 @@ ActiveRecord::Schema.define(version: 2023_12_12_161831) do
     t.boolean "with_pets"
     t.string "first_name"
     t.string "last_name"
-    t.bigint "property_id", null: false
+    t.integer "property_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "message"
