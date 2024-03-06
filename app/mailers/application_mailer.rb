9d365e82d08539dd5a6e9876910b96d62a9f5fe7
Kat W-E <kat@fonix.com>
2024-03-06 16:50:35 +0000

    image gallery updates and sending from name added

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@margatekipndip.co.uk'
+  default from: email_address_with_name('no-reply@margatekipndip.co.uk', 'Margate Kip n Dip')
   layout 'mailer'
 end
