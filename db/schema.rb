29002c12a10cf1ecf7ccce7b65e0c2efc74aaa03
Kat W-E <kat@fonix.com>
2025-03-30 10:00:29 +0100

    add to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(version: 2024_04_14_124617) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Property.find_or_create_by!(address: "Temeraire Court", title: "Penthouse flat hosted by Chris", description: "Hi I'm Chris, welcome to my flat...", price_per_night: 100, minimum_nights: 3, cleaning_fee: 50, max_guests: 4, bedrooms: 2, bathrooms: 2, pet_friendly: true)
+
