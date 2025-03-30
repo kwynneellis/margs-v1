0131f36d8c53cb2ce2becf6ac95bef8806006579
Kat W-E <kat@fonix.com>
2025-03-30 10:35:16 +0100

    adds nights of stay validation to enquiries

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -4,8 +4,14 @@ class Enquiry < ApplicationRecord
   validates :first_name, :last_name, :email, :mobile, :check_in, :check_out, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :mobile, :presence => true, :numericality => true, :length => { :minimum => 11, :maximum => 15 }
+  validates :nights_of_stay, numericality: { greater_than_or_equal_to: 1 }
+  validates_comparison_of :check_out, greater_than: :check_in, other_than: Date.today
 
   after_create :send_thanks_email, :send_new_enquiry_email
+
+  def nights_of_stay
+    (check_out - check_in).to_i
+  end
 
   private
 
