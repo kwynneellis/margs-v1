19d2e3fa24341dc7abcabd9176028839c93c6436
Kat W-E <kat@fonix.com>
2025-03-30 11:12:38 +0100

    remove :new from enquiries in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :properties, only: [:index, :show, :edit, :update] do
     get "/get-in-touch-2317", to: 'enquiries#new', as: :new_enquiry
-    resources :enquiries, only: [:new, :create, :index, :show, :destroy]
+    resources :enquiries, only: [:create, :index, :show, :destroy]
     resources :recommendations, only: [:new, :create, :index, :edit, :update, :destroy]
     resources :bookings, only: [:new, :create, :index, :edit, :update, :destroy]
   end
